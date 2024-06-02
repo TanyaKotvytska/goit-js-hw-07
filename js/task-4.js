5a29cc0680db7c8107cf93267e59c8f857e95351
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-06-02 13:06:01 +0300

    add new code

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const form = event.target;
+    const emailInput = form.querySelector("input[name='email']").value.trim();
+    const passwordInput = form.querySelector("input[name='password']").value.trim();
+
+    if (emailInput === "" || passwordInput === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+
+    const formData = {
+        email: emailInput,
+        password: passwordInput
+    };
+
+    console.log(formData);
+    form.reset();
+});
